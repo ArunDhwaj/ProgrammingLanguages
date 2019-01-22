45ead5bfd58fa00646fd0c1bf19e4dec8995d69e
spb.arun.01@gmail.com <arun.dhwaj@smartbitpixel.co.in>
2019-01-22 17:48:24 +0530

    File is renamed

---
+++ SpringBoot/RestFullAPI/src/main/java/com/sbpjson/SpbjsonApplication.java
@@ -0,0 +1,14 @@
+package com.sbpjson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbjsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbjsonApplication.class, args);
+	}
+
+}
+
